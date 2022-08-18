8d4bd87eb97031a581fe4afd31bc620c21398e4a
Felipe Barcon <felipe.barcon@gmail.com>
2022-08-18 11:05:58 +0200

    starting traductions

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from '../src/App/App';
 import reportWebVitals from './reportWebVitals';
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
+import { IntlProvider } from 'react-intl';
+import traductions from "./traductions";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// Traduction
+
+const langue = navigator.language.substring(0, 2);
+let messages = traductions.fr;
+switch (langue) {
+    case "en": messages= traductions.en; break;
+    case "es": messages= traductions.es; break;
+}
 root.render(
-  <React.StrictMode>
-    <DevSupport ComponentPreviews={ComponentPreviews}
-                useInitialHook={useInitial}
-    >
+    <IntlProvider locale={langue} messages={messages}>
       <App />
-    </DevSupport>
-  </React.StrictMode>
+    </IntlProvider>
 );
 
 reportWebVitals();
---
+++ src/traductions.ts
@@ -0,0 +1,27 @@
+const traductions = {
+    fr: {
+        'formulaire.nom': 'Nom',
+        'formulaire.age': 'Age',
+        'formulaire.Homme': 'Homme',
+        'formulaire.Femme': 'Femme',
+        'formulaire.submit': 'Envoyer',
+    },
+
+    en: {
+        'formulaire.nom': 'Name',
+        'formulaire.age': 'Age',
+        'formulaire.Homme': 'Male',
+        'formulaire.Femme': 'Female',
+        'formulaire.submit': 'Send',
+    },
+
+    es: {
+        'formulaire.nom': 'Nombre',
+        'formulaire.age': 'Edad',
+        'formulaire.Homme': 'Hombre',
+        'formulaire.Femme': 'Mujer',
+        'formulaire.submit': 'Enviar',
+    }
+}
+
+export default traductions;
